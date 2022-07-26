54d2b8897595c6a6e928815fa4fc8e900195ced1
razvan <razvanghinea05@gmail.com>
2022-07-26 12:58:17 +0300

    add request middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 import usersController from "../controllers/userController.js";
+import requestMiddleware from "../middleware/requestMiddleware.js";
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 const router = express.Router();
 
-router.get("/", usersController.getUsers);
-router.get("/:id(\\d+)", usersController.getUser);
+router.get("/", requestMiddleware, usersController.getUsers);
+router.get("/:id(\\d+)", requestMiddleware, usersController.getUser);
 router.get("/test", usersController.fakeGetUser2);
 router.get("/:id(\\w+)", usersController.fakeGetUser);
 
